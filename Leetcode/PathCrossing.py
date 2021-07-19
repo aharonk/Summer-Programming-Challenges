e21ed6c6f78b9758dac2494714c5db8c3cbbda41
Aharon Katz <aharon@ymkatz.net>
2021-07-19 17:04:47 -0400

    Added comments

--- Leetcode/PathCrossing.py
+++ Leetcode/PathCrossing.py
@@ -8,6 +8,7 @@ class Solution:
 
         for char in path:
 
+            # based on the current character, change the location we are currently in.
             if char == 'N':
                 curr_coords[0] += 1
             elif char == 'S':
@@ -17,13 +18,16 @@ class Solution:
             elif char == 'W':
                 curr_coords[1] -= 1
 
+            # create a tuple to add to the set, in the occasion the set does not already contain the current position. If it does, we will return True
             if (tuple_coords := tuple(curr_coords)) in coords_visited:
                 return True
 
             coords_visited.add(tuple_coords)
 
+        # if none of the coordinates matched up, return that the line never met itself
         return False
 
+# testing
 for string, result in {"NES": False, "NESWW": True, "NNSWWEWSSESSWENNW": True, "": False}.items():
     s = Solution(string)
     print(result)
